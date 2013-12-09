5292c6237654577a3aba6bd695ae9b69b6e4e56a
tricom7@gmail.com <tricom7@gmail.com@c2fe57ba-39af-3040-bb6e-2510577d8c77>
2013-12-09 07:28:39 +0000

    sound 문제 해결

---
+++ pgtest03/platforms/android/bin/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+C:\workspace_atd2\pgtest05\platforms\android\bin\AndroidManifest.xml : \
+C:\workspace_atd2\pgtest05\platforms\android\AndroidManifest.xml \
---
+++ pgtest03/platforms/android/bin/HelloWorld-debug-unaligned.apk.d
@@ -0,0 +1,4 @@
+C:\workspace_atd2\pgtest05\platforms\android\bin\HelloWorld-debug-unaligned.apk : \
+C:\workspace_atd2\pgtest05\platforms\android\bin\HelloWorld.ap_ \
+C:\workspace_atd2\pgtest05\platforms\android\bin\classes.dex \
+C:\workspace_atd2\pgtest05\platforms\android\libs\cordova-3.1.0.jar \
