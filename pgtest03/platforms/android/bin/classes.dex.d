d31fb5daeb66a8f46cd3b627b9658214ff1f7f9c
tricom7 <tricom7@gmail.com>
2013-12-09 07:28:39 +0000

    sound 문제 해결

---
+++ pgtest03/platforms/android/bin/classes.dex.d
@@ -0,0 +1,15 @@
+C:\workspace_atd2\pgtest05\platforms\android\bin\classes.dex : \
+C:\workspace_atd2\pgtest05\platforms\android\bin\classes\com\phonegap\helloworld\BuildConfig.class \
+C:\workspace_atd2\pgtest05\platforms\android\bin\classes\com\phonegap\helloworld\HelloWorld.class \
+C:\workspace_atd2\pgtest05\platforms\android\bin\classes\com\phonegap\helloworld\R$attr.class \
+C:\workspace_atd2\pgtest05\platforms\android\bin\classes\com\phonegap\helloworld\R$drawable.class \
+C:\workspace_atd2\pgtest05\platforms\android\bin\classes\com\phonegap\helloworld\R$string.class \
+C:\workspace_atd2\pgtest05\platforms\android\bin\classes\com\phonegap\helloworld\R$xml.class \
+C:\workspace_atd2\pgtest05\platforms\android\bin\classes\com\phonegap\helloworld\R.class \
+C:\workspace_atd2\pgtest05\platforms\android\bin\classes\org\apache\cordova\media\AudioHandler.class \
+C:\workspace_atd2\pgtest05\platforms\android\bin\classes\org\apache\cordova\media\AudioPlayer$1.class \
+C:\workspace_atd2\pgtest05\platforms\android\bin\classes\org\apache\cordova\media\AudioPlayer$MODE.class \
+C:\workspace_atd2\pgtest05\platforms\android\bin\classes\org\apache\cordova\media\AudioPlayer$STATE.class \
+C:\workspace_atd2\pgtest05\platforms\android\bin\classes\org\apache\cordova\media\AudioPlayer.class \
+C:\workspace_atd2\pgtest05\platforms\android\bin\classes\org\apache\cordova\media\FileHelper.class \
+C:\workspace_atd2\pgtest05\platforms\android\bin\dexedLibs\cordova-3.1.0-339d70d86a6bed17896d0c7d2a7362ca.jar \
---
+++ pgtest03/platforms/android/gen/R.java.d
@@ -0,0 +1,9 @@
+C:\workspace_atd2\pgtest05\platforms\android\gen\com\phonegap\helloworld\R.java \
+ : C:\workspace_atd2\pgtest05\platforms\android\res\drawable\icon.png \
+C:\workspace_atd2\pgtest05\platforms\android\res\drawable-hdpi\icon.png \
+C:\workspace_atd2\pgtest05\platforms\android\res\drawable-ldpi\icon.png \
+C:\workspace_atd2\pgtest05\platforms\android\res\drawable-mdpi\icon.png \
+C:\workspace_atd2\pgtest05\platforms\android\res\drawable-xhdpi\icon.png \
+C:\workspace_atd2\pgtest05\platforms\android\res\values\strings.xml \
+C:\workspace_atd2\pgtest05\platforms\android\res\xml\config.xml \
+C:\workspace_atd2\pgtest05\platforms\android\bin\AndroidManifest.xml \
